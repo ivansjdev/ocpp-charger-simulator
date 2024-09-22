bf4fd998c6daa3f69d6252f735ca71eda5aaa701
ivan <i.sanjuantejedor@gmail.com>
2024-09-22 11:38:59 +0200

    :lipstick: add next-themes basic implementation and center all

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { ThemeProvider } from "@/components/theme/theme-provider";
+import { ThemeSwitcher } from "@/components/theme/theme-switcher";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -19,17 +21,38 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
+                                     children,
+                                   }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <body
+      className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+    >
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <div className="flex flex-col items-center">
+        <header className='flex justify-between w-full max-w-4xl pt-2 pb-5 pl-5 pr-5'>
+          <div className='flex items-center'>
+            <h2>OCPP Charging Station Simulator</h2>
+            {/* Otros elementos futuros a la izquierda */}
+          </div>
+          <div className='flex items-center'>
+            {/* Otros elementos futuros a la derecha */}
+            <ThemeSwitcher />
+          </div>
+        </header>
+        <main className="w-full max-w-4xl p-5">
+          {children}
+        </main>
+      </div>
+    </ThemeProvider>
+    </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import ChargerCard from "@/components/cards/charger-card";
+import { ModeToggle } from "@/components/theme/theme-switcher";
 
 export default function Home() {
   return (
     <div className="">
       <ChargerCard />
+
     </div>
   );
 }
